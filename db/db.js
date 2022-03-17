8fb1ea7675c23209021fe7cc8bab3e9f4762934b
Rony Hajong <ronyhajong2000@gmail.com>
2022-03-17 06:06:51 +0600

    db

---
+++ db/db.js
@@ -0,0 +1,119 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				
+			],
+		},
+		contact: {
+			text: [
+				"Email Address :ronyhajong2000@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "C, C++",
+			color: "1",
+			percentage: "80",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Bootstrap",
+			color: "6",
+			percentage: "50",
+		},
+		{
+			title: "Backend",
+			skillName: "PHP,MySQL",
+			color: "2",
+			percentage: "70",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, CSS",
+			color: "4",
+			percentage: "70",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub",
+			color: "7",
+			percentage: "70",
+		},
+		{
+			title: "Editor",
+			skillName: "VS Code,Atom,Codeblocks",
+			color: "6",
+			percentage: "70",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "Hall Management",
+				image: "images/hall.jpg",
+				summary:
+					"Hall Management System for SUST student residents",
+				preview: "https://github.com/cse-250-2018/G25-Hall-Management-Web",
+				techStack: [
+					"HTML",
+					"CSS",
+					"Bootstrap",
+					"JavaScript",
+					"PHP",
+					"MySQL",
+				],
+			},	
+		],
+	},
+	experience: [
+		
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com/users/15122049/rony-hajong",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/rony2510",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/letxequal0/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/rony2510/",
+				},
+				{
+					text: "Twitter",
+					link: "https://twitter.com/letxequal0",
+				},
+			],
+		},
+	],
+};
